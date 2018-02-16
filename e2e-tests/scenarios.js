fe4201aa01669d5c77702475dcc47b74ef8ef3a4
Sangeetha <sangeetha_sivaramakrishnan1@homedepot.com>
2018-02-16 17:25:14 -0500

    wip: additional e2e tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,7 +1,7 @@
 describe('Protractor Demo App', function() {
 
     beforeEach(function() {
-       // browser.get('hhttp://localhost:8000');
+        //browser.get('hhttp://localhost:8000');
     });
 
 
@@ -25,6 +25,34 @@ describe('Protractor Demo App', function() {
         query.sendKeys('snippet with');
         expect(phoneList.count()).toBe(3);
 
+    });
+
+
+    it('should be possible to control phone order via the drop-down menu', function() {
+        var queryField = element(by.model('$ctrl.query'));
+        var orderSelect = element(by.model('$ctrl.orderProp'));
+        //var nameOption = orderSelect.element(by.css('option[value="name"]'));
+        var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+
+        function getNames() {
+            return phoneNameColumn.map(function(elem) {
+                return elem.getText();
+            });
+        }
+
+        queryField.sendKeys('phone');   // Let's narrow the dataset to make the assertions shorter
+
+        expect(getNames()).toEqual([
+            'iPhone',
+            'Windows Phone'
+        ]);
+
+        nameOption.click();
+
+        expect(getNames()).toEqual([
+            'iPhone',
+            'Windows Phone'
+        ]);
     });
 
 });
