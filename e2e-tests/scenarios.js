a62ede8b1919c841fa3ed9c66b9ac197da593345
Sivaramakrishnan <sxs1208@d25t6073gq17im.amer.homedepot.com>
2018-02-20 16:53:36 -0500

    chapter 8

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -37,9 +37,9 @@ describe('Protractor Demo App', function() {
         //element.all(by.css('ul li')).then(function(items) {
         $$('ul li').then(function(phones) {
             expect(phones.length).toBe(20);
-            var name1 = phones[0].element(by.css('span'));
-            var name2 = phones[1].element(by.css('span'));
-            var name3 = phones[2].element(by.css('span'));
+            var name1 = phones[0].element(by.css('span a'));
+            var name2 = phones[1].element(by.css('span a'));
+            var name3 = phones[2].element(by.css('span a'));
             expect(name1.getText()).toBe('Motorola XOOM™ with Wi-Fi');
             expect(name2.getText()).toBe('MOTOROLA XOOM™');
             expect(name3.getText()).toBe('MOTOROLA ATRIX™ 4G');
@@ -51,8 +51,8 @@ describe('Protractor Demo App', function() {
         //element.all(by.css('ul li')).then(function(items) {
         $$('ul li').then(function(phones) {
             expect(phones.length).toBe(5);
-            var name1 = phones[0].element(by.css('span'));
-            var name2 = phones[1].element(by.css('span'));
+            var name1 = phones[0].element(by.css('span a'));
+            var name2 = phones[1].element(by.css('span a'));
 
             expect(name1.getText()).toBe('Samsung Gem™');
             expect(name2.getText()).toBe('Samsung Galaxy Tab™');
@@ -70,9 +70,9 @@ describe('Protractor Demo App', function() {
         //element.all(by.css('ul li')).then(function(items) {
         $$('ul li').then(function(phones) {
             expect(phones.length).toBe(20);
-            var name1 = phones[0].element(by.css('span'));
-            var name2 = phones[1].element(by.css('span'));
-            var name3 = phones[2].element(by.css('span'));
+            var name1 = phones[0].element(by.css('span a'));
+            var name2 = phones[1].element(by.css('span a'));
+            var name3 = phones[2].element(by.css('span a'));
             expect(name1.getText()).toBe('Motorola XOOM™ with Wi-Fi');
             expect(name2.getText()).toBe('MOTOROLA XOOM™');
             expect(name3.getText()).toBe('MOTOROLA ATRIX™ 4G');
@@ -84,9 +84,9 @@ describe('Protractor Demo App', function() {
         //element.all(by.css('ul li')).then(function(items) {
         $$('ul li').then(function(phones) {
             expect(phones.length).toBe(20);
-            var name1 = phones[0].element(by.css('span'));
-            var name2 = phones[1].element(by.css('span'));
-            var name3 = phones[2].element(by.css('span'));
+            var name1 = phones[0].element(by.css('span a'));
+            var name2 = phones[1].element(by.css('span a'));
+            var name3 = phones[2].element(by.css('span a'));
 
             expect(name1.getText()).toBe('Dell Streak 7');
             expect(name2.getText()).toBe('Dell Venue');
@@ -94,5 +94,17 @@ describe('Protractor Demo App', function() {
 
         });
 
+    });
+
+    it('should render phone specific links', function() {
+
+        browser.get('http://localhost:8000');
+
+        var queryField = element(by.model('$ctrl.query'));
+
+        queryField.sendKeys('nexus');
+
+        element.all(by.css('.phones li a')).first().click();
+        expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
     });
 });
